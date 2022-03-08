b49979129f00084c9ba86627b19738a9a37a7d75
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-03-09 01:01:27 +0700

    Latihan 1.10

---
+++ Liang - Introduction in Progamming Using Python/Chapter 01/Latihan 1.10.py
@@ -0,0 +1,22 @@
+# =============================================
+# Menentukan Kecepatan Rata-rata
+# =============================================
+
+jarakKm = 14
+jarakMile = 1.6 * 14
+waktuMenit = 45
+waktuDetik = 30
+waktuJam = (waktuMenit / 60) + (waktuDetik / 3600)
+kecepatan = jarakMile / waktuJam
+
+print("=============================================")
+print("Menentukan Kecepatan Rata-rata")
+print("---------------------------------------------")
+print("Kecepatan rata-rata dengan,")
+print("jarak = 14 Km")
+print("waktu = 45 menit 30 detik")
+print("adalah,")
+print(kecepatan)
+print("(dalam mil/jam)")
+print("=============================================")
+
