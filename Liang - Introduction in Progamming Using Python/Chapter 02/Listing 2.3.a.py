a62f843d4b24b53f4b737a47a552aaddbe13c037
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-03-31 00:40:41 +0700

    Listing 2.3.a

---
+++ Liang - Introduction in Progamming Using Python/Chapter 02/Listing 2.3.a.py
@@ -0,0 +1,9 @@
+# Compute the first area
+radius = 1.0
+area = radius * radius * 3.14159
+print("The area is", area, "for radius", radius)
+
+# Compute the second area
+radius = 2.0
+area = radius * radius * 3.14159
+print("The area is", area, "for radius", area)
