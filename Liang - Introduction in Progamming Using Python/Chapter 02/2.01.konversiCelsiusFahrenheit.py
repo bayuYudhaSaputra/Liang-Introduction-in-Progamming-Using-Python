59ab7398d0f64624092e26cbb6a554e376485554
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-04-19 00:35:51 +0700

    2.01.konversiCelsiusFahrenheit

---
+++ Liang - Introduction in Progamming Using Python/Chapter 02/2.01.konversiCelsiusFahrenheit.py
@@ -0,0 +1,21 @@
+# =========================================================================================
+# Konversi Celsius ke Fahrenheit
+# -----------------------------------------------------------------------------------------
+# Step 1. Mendefinisikan variabel celsius menggunakan user input.
+# Step 2. Menghitung nilai fahrenheit dengan rumus fahrenheit = (9 / 5) * celsius + 32
+# Step 3. Tampilkan nilai fahrenheit
+# =========================================================================================
+
+# Step 1
+# --------
+celsius = eval(input("Ketikkan suhu dalam Celsius :"))
+
+# Step 2
+# --------
+fahrenheit = (9 / 5) * celsius + 32
+
+# Step 3
+# --------
+print("Suhu", celsius, "derajat celsius sama dengan", fahrenheit, "derajat fahrenheit.")
+
+# =========================================================================================
