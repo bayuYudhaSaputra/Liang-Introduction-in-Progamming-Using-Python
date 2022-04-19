f632fd403e56de4fe54427533f7c79bbcae9f590
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-04-19 22:31:04 +0700

    2.03.konversiKakiMeter

---
+++ Liang - Introduction in Progamming Using Python/Chapter 02/2.03.konversiKakiMeter.py
@@ -0,0 +1,17 @@
+# =======================================================================
+# Konversi Satuan Kaki ke Meter
+# ------------------------------------------------------------------------
+# Langkah 1. Input ukuran dalam satuan kaki
+# Langkah 2. Hitung ukuran dalam meter dengan rumus meter = 0.305 * kaki
+# Langkah 3. Tampilkan ukuran dengan satuan meter
+# ------------------------------------------------------------------------
+
+# Langkah 1. Input ukuran dalam satuan kaki
+kaki = eval(input("Input ukuran dalam satuan kaki :"))
+
+# Langkah 2. Hitung ukuran dalam meter dengan rumus meter = 0.305 * kaki
+meter = 0.305 * kaki
+
+# Langkah 3. Tampilkan ukuran dengan satuan meter
+print(kaki, "kaki sama dengan", meter, "meter")
+# =======================================================================
