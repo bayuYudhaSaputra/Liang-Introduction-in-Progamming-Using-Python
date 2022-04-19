7a47d151831bf3d342982b5e6c12368333412896
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-04-19 23:01:57 +0700

    2.04.konversiPonKg

---
+++ Liang - Introduction in Progamming Using Python/Chapter 02/2.04.konversiPonKg.py
@@ -0,0 +1,18 @@
+# ===============================================================================================
+# Konversi Pon ke Kilogram
+# ------------------------------------------------------------------------------------------------
+# Langkah 1. input ukuran dalam satuan pon
+# Langkah 2. Hitung ukuran tersebut dengan satuan kilogram dengan rumus : kilogram = 0.454 * pon
+# Langkah 3. Tampilkan ukuran dengan satuan kilogram
+# ------------------------------------------------------------------------------------------------
+
+# Langkah 1. input ukuran dalam satuan pon
+pon = eval(input("Input ukuran dengan satuan pon :"))
+
+# Langkah 2. Hitung ukuran tersebut dengan satuan kilogram
+kilogram = 0.454 * pon
+
+# Langkah 3. Tampilkan ukuran dengan satuan kilogram
+print(pon, "pon sama dengan", kilogram, "Kg")
+
+# ===============================================================================================
