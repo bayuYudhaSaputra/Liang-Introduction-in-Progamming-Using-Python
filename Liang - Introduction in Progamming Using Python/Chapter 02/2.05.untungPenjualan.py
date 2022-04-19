bb8aa76b252a710e9c9c2870603828a5f0ad6cb5
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-04-19 23:43:19 +0700

    2.05.untungPenjualan

---
+++ Liang - Introduction in Progamming Using Python/Chapter 02/2.05.untungPenjualan.py
@@ -0,0 +1,28 @@
+# ==================================================================================
+# Hitung Harga Penjualan
+# ----------------------------------------------------------------------------------
+# Langkah 1. Input harga pembelian dan persentase untung
+# Langkah 2. Hitung untung dengan rumus : untung = (persenUntung / 100) * hargaBeli
+# Langkah 3. Hitung harga penjualan dengan rumus : hargaJual = hargaBeli + untung
+# Langkah 4. Tampilkan harga penjualan
+# ----------------------------------------------------------------------------------
+
+# Langkah 1. Input harga pembelian dan persentase untung
+hargaBeli, persenUntung = eval(input("Input Harga beli (misal. Rp100000) dan persentase untung (misal. 5%) :"))
+
+# Langkah 2. Hitung untung
+untung = (persenUntung / 100) * hargaBeli
+
+# Langkah 3. Hitung harga penjualan
+hargaJual = hargaBeli + untung
+
+# Langkah 4. Tampilkan harga penjualan
+print(
+    "Jika barang dibeli dengan harga Rp", 
+    hargaBeli,
+    "dengan untung",
+    persenUntung, 
+    "%",
+    "maka harga jualnya adalah : Rp",
+    hargaJual
+    )
