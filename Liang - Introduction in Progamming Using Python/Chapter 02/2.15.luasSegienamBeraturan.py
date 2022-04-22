3290da39795467bb492c948db94448e4992975a9
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-04-23 01:24:06 +0700

    2.15.luasSegienamBeraturan

---
+++ Liang - Introduction in Progamming Using Python/Chapter 02/2.15.luasSegienamBeraturan.py
@@ -0,0 +1,20 @@
+# ======================================================================
+# Luas segienam beraturan
+# ----------------------------------------------------------------------
+
+# Langkah 1. Input panjang sisi segienam beraturan
+panjangSisi = eval(input("Input panjang sisi segienam beraturan :"))
+
+# Langkah 2. Hitung luas segienam beraturan dengan rumus :
+luasSegienamBeraturan = (( 3 * 3 ** 0.5 ) / 2) * panjangSisi ** 2
+
+# Langkah 3. Tampilkan luas segienam beraturan
+print(
+        "Luas segienam beraturan dengan panjang sisi",
+        panjangSisi,
+        "satuan panjang adalah :",
+        luasSegienamBeraturan,
+        "satuan luas"
+    )
+
+# =======================================================================
