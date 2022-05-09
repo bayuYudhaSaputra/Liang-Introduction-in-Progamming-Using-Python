a78a10d1ac0703938dd7f41a0aca1f8ad2f721f5
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-05-10 05:32:49 +0700

    Listing 3.1. MathFunctions

---
+++ Liang - Introduction in Progamming Using Python/Chapter 03/Listing 3.1.mathFunctin.py
@@ -0,0 +1,31 @@
+# ===============================================================
+# Mathematic Function
+# ===============================================================
+
+# Langkah 1 : impor modul Math
+import math
+
+# Langkah 2 : Test Algebraic Function
+print("-----------------------------------------------")
+print("Hasil Fungsi-fungsi Aljabar")
+print("-----------------------------------------------")
+
+print("Nilai dari exp(1) = ", math.exp(1))
+print("Nilai dari log(2.8) = ", math.log(2.8))
+print("Nilai dari log10(10) = ", math.log(10, 10))
+print("Nilai sqrt(4) = ", math.sqrt(4))
+
+print("-----------------------------------------------")
+
+# Langkah 3 : Test fungsi-fungsi trigonometri
+print("-----------------------------------------------")
+print("Fungsi-fungsi Trigonometri")
+print("-----------------------------------------------")
+
+print("Nilai sin( PI / 2) = ", math.sin( math.pi / 2 ))
+print("Nilai cos( PI / 2) = ", math.cos( math.pi / 2 ))
+print("Nilai tan( PI / 2) = ", math.tan( math.pi / 2 ))
+print("Nilai degree( 1.57 ) = ", math.degrees( 1.57 ))
+print("Nilai radian( 90 ) = ", math.radians( 90 ))
+
+print("-----------------------------------------------")
