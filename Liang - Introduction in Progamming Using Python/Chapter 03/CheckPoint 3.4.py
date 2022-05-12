4d2c184337449c40ebbaf6c0f4efb3022c4b4efe
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-05-12 17:04:34 +0700

    CheckPoint 3.4.

---
+++ Liang - Introduction in Progamming Using Python/Chapter 03/CheckPoint 3.4.py
@@ -0,0 +1,15 @@
+# =============================================
+# Konversi Radian ke Derajat
+# =============================================
+
+# Import modul math
+import math
+
+# definisikan sudut radian
+sudutRadian = math.pi / 7
+
+# konversi sudut radian ke sudut radian
+sudutDerajat = math.degrees(sudutRadian)
+
+# tampilkan hasil konversi
+print("Pi / 7 radian = ", sudutDerajat, "derajat")
