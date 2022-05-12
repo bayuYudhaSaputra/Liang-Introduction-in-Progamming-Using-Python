e745973bf34f84b34a0c522eb0c38271c58de9a5
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-05-12 10:37:25 +0700

    CheckPoint 3.3

---
+++ Liang - Introduction in Progamming Using Python/Chapter 03/CheckPoint 3.1.py
@@ -0,0 +1,13 @@
+# =========================================================
+# Ubah derajat ke radian
+# =========================================================
+
+import math
+from turtle import radians
+
+sudutDerajat = 47
+sudutRadian = math.radians(sudutDerajat)
+
+print(sudutDerajat, "derajat = ", sudutRadian, "radian.")
+
+# =========================================================
