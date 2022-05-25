ae7c28b663601804f484424f30ef4fe9999e3643
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-05-26 01:09:23 +0700

    Listing 3.6.ColorShapes.py

---
+++ Liang - Introduction in Progamming Using Python/Chapter 03/Listing 3.6.ColorShapes.py
@@ -0,0 +1,58 @@
+# ========================================
+# Turtle menggunakan warna
+# ========================================
+
+import turtle
+turtle.pensize(3)
+
+# 1. Menggambar Segitiga Berwarna Merah
+turtle.penup()
+turtle.goto(-200,-50)
+turtle.pendown()
+turtle.pendown()
+turtle.begin_fill()
+turtle.color("red")
+turtle.circle(40, steps = 3)
+turtle.end_fill()
+
+# 2. Menggambar Segiempat Berwarna Biru
+turtle.penup()
+turtle.goto(-100,-50)
+turtle.pendown()
+turtle.pendown()
+turtle.begin_fill()
+turtle.color("blue")
+turtle.circle(40, steps = 4)
+turtle.end_fill()
+
+# 3. Menggambar Segilima Berwarna Hijau
+turtle.penup()
+turtle.goto(0,-50)
+turtle.pendown()
+turtle.pendown()
+turtle.begin_fill()
+turtle.color("green")
+turtle.circle(40, steps = 5)
+turtle.end_fill()
+
+# 4. Menggambar Segienam Berwarna Kuning
+turtle.penup()
+turtle.goto(100,-50)
+turtle.pendown()
+turtle.pendown()
+turtle.begin_fill()
+turtle.color("yellow")
+turtle.circle(40, steps = 6)
+turtle.end_fill()
+
+# 5. Menggambar Lingkaran Berwarna Ungu
+turtle.penup()
+turtle.goto(200,-50)
+turtle.pendown()
+turtle.pendown()
+turtle.begin_fill()
+turtle.color("purple")
+turtle.circle(40)
+turtle.end_fill()
+
+turtle.done()
