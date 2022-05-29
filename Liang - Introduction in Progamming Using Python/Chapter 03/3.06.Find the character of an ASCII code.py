423a2c7bccd9f853311d901a6f88777d7aca42f4
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-05-29 11:50:09 +0700

    3.06.Find the character of an ASCII cod

---
+++ Liang - Introduction in Progamming Using Python/Chapter 03/3.06.Find the character of an ASCII code.py
@@ -0,0 +1,16 @@
+# =======================================================
+# Mengubah kode ASCII menjadi karakter
+# =======================================================
+
+# 1. Input kode ASCII
+kodeASCII = eval(input("Input kode ASCII : "))
+
+# 2. Ubah kode ASCII menjadi karakter
+karakterKonversi = chr(kodeASCII)
+
+# 3. Tampilkan hasil pengubahan kode ASCII
+print("Karakter untuk kode ASCII",
+        kodeASCII,
+        "adalah",
+        karakterKonversi
+)
