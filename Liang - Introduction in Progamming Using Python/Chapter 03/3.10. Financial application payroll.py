dcd57d84231d8160c897294c7f3af3386d6e4b5f
bayuYudhaSaputra <65967960+bayuYudhaSaputra@users.noreply.github.com>
2022-06-01 22:52:45 +0700

    3.10. Turtle: display Unicodes

---
+++ Liang - Introduction in Progamming Using Python/Chapter 03/3.10. Financial application payroll.py
@@ -0,0 +1,11 @@
+# =====================================
+# Menampilkan abjad Yunani ke Turtle
+# =====================================
+
+import turtle
+
+turtle.write("\u03b1 \u03b2 \u03b3 \u03b4 \u03b7 \u03b8 \u03b5 \u03b6")
+
+turtle.hideturtle()
+
+turtle.done()
