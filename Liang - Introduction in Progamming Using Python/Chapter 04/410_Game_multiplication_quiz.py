43c855fae7a46f53acae5055b2bd929fa114ae39
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-07-06 00:52:42 +0700

    4.10.Game: multiplication quiz

--- Liang - Introduction in Progamming Using Python/Chapter 04/410_Game_multiplication_quiz.py
+++ Liang - Introduction in Progamming Using Python/Chapter 04/410_Game_multiplication_quiz.py
@@ -1,16 +1,15 @@
+import random
 # ===============================
 # Membuat Hasil Perkalian Acak
 # ================================
 
 print("===============================================")
-import random
+
 # membuat bilangan random antara 0 hingga 100
 bil1 = random.randint(0,99)
 bil2 = random.randint(0,99)
 
-jawabanUser = eval(input("Berapakah hasil " + \
-            str(bil1) + " * " + str(bil2) + " ? "
-    ))
+jawabanUser = eval(input("Berapakah hasil " + str(bil1) + " * " + str(bil2) + " ? " ))
 
 # Cek jawaban user dengan hasil perkalian
 if jawabanUser == bil1 * bil2:
