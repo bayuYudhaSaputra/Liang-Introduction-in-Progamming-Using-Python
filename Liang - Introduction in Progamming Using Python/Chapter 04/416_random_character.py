ad85491533f40bd0155179fab6b164d4cedd50dc
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-07-10 00:54:30 +0700

    4.16. Random Character

---
+++ Liang - Introduction in Progamming Using Python/Chapter 04/416_random_character.py
@@ -0,0 +1,7 @@
+import random
+
+# membuat indeks ASCII huruf kapital secara random
+indeksASCII = random.randint(64,90)
+
+# menampilkan huruf kapital random
+print("Hasil random adalah : ", chr(indeksASCII))
