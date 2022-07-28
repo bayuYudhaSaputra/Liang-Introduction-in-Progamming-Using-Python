110590032b1cc50d72f0c23b89f562eb6b308096
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-07-28 10:49:33 +0700

    Add 52a

---
+++ Liang - Introduction in Progamming Using Python/Chapter 05/52a_repeat_additions.py
@@ -0,0 +1,53 @@
+import random
+import time
+
+# Count the number of correct answers
+correctCount = 0
+
+# Count the number of questions
+countAnswer = 0
+
+# 5 question only
+NUMBER_OF_QUESTIONS = 10
+
+print("=========================================================== \n",
+    "Adding Quiz \n",
+    "-------------------------------------------------------------"
+    )
+
+# Get start time
+startTime = time.time()
+
+while countAnswer < NUMBER_OF_QUESTIONS:
+    # Generate two random single digit integers
+    number1 = random.randint(0,9)
+    number2 = random.randint(0,9)
+
+    # Prompt the student to answer "What is number1 - number2 ?"
+    answer = eval(input(str(countAnswer + 1) + ". What is " + str(number1) + \
+        " + " + str(number2) + "? "
+        ))
+    
+    # Grade the answer and display the result
+    if number1 + number2 == answer:
+        print("Response : \n","\t You are correct! \n")
+        correctCount += 1
+    else:
+        print("Response : \n"
+            "\t Your answer is wrong \n",
+            "\t", number1, "+", number2, " = ",number1 + number2, "\n"
+            )
+    # Increase the count
+    countAnswer += 1
+
+# Get end time
+endTime = time.time()
+
+print("-------------------------------------------------------------")
+# Get test time
+testTime = int(endTime - startTime)
+print("Correct count is", correctCount,
+    "out of", NUMBER_OF_QUESTIONS,
+    "\nTest time is", testTime, "second"
+    )
+print("===========================================================")
