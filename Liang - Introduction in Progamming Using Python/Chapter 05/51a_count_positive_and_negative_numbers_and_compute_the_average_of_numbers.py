55d92d39fc769108e014e174b38dcc7e247773be
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-04 17:01:51 +0700

    add 5.1a

---
+++ Liang - Introduction in Progamming Using Python/Chapter 05/51a_count_positive_and_negative_numbers_and_compute_the_average_of_numbers.py
@@ -0,0 +1,29 @@
+# =========================================================================
+# Count Positive and Negative Numbers and Compute the Average of Numbers
+# -------------------------------------------------------------------------
+
+print("================================================================== \n",
+    "Menghitung jumlah dan rata-rata bilangan positif dan negatif \n",
+    "--------------------------------------------------------------------"
+    )
+inputBilangan = eval(input("Input bilangan integer" + 
+                "(program akan berhenti jika anda menginput 0) : "))
+
+sum = 0
+banyakPerulangan = 0
+banyakNegatif = 0
+
+while inputBilangan != 0:
+    sum += inputBilangan
+    banyakPerulangan += 1 # menghitung banyak perulangan
+    rata2 = sum / banyakPerulangan
+    
+    if inputBilangan < 0:
+        banyakNegatif += 1 # menghitung banyak bilangan negatif yang diinput
+    
+    inputBilangan = eval(input("Input bilangan integer" + 
+                "(program akan berhenti jika anda menginput 0) : "))
+
+print("Jumlah bilangan yang diinput adalah", sum)
+print("Rata-rata bilangan yang diinput adalah", rata2)
+print("Banyak bilangan negatif yang diinput adalah", banyakNegatif)
