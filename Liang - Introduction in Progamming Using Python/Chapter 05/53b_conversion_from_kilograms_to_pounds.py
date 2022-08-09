8d7c48ee495e7e154b12b547728e6cad6cce6f1e
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-10 01:12:06 +0700

    add 53b

---
+++ Liang - Introduction in Progamming Using Python/Chapter 05/53b_conversion_from_kilograms_to_pounds.py
@@ -0,0 +1,25 @@
+# =========================================
+# Tabel Konversi Kg ke Pound
+# -----------------------------------------
+
+print(" ============================= \n",
+    "Tabel Konversi Kg ke Pound \n",
+    "--------------------------"
+    )
+
+print(
+    " ======================= \n",
+    " |", format("No.","3s"),"|", format("Kg ","3s"), "|", format("Pon","6s"), "|\n",
+    "-----------------------"
+    )
+
+kilogram = 1
+
+for nomor in range(1,100):
+    pound = 2.2 * kilogram
+    
+    print(
+    "  |", format(nomor,"3"), "|", format(kilogram,"3"), "|", format(pound, "6.2f"), "|"
+    )
+
+    kilogram = 2 * nomor - 1 # rumus barisan bilangan ganjil
