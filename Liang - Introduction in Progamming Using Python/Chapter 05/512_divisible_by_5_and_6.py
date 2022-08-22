29f4f201abad4b4370f6a1947f06d6d79b4ab6d3
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-22 19:09:42 +0700

    change 512

--- Liang - Introduction in Progamming Using Python/Chapter 05/512_divisible_by_5_and_6.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/512_divisible_by_5_and_6.py
@@ -1,7 +1,11 @@
-BANYAK_BILANGAN_PER_BARIS = 10
-bilangan = 100
+print("----------------------------------------------------------\n",
+    "Bilangan antara 100 hingga 1000 yang habis dibagi 5 dan 6\n",
+    "----------------------------------------------------------"
+    )
+for i in range(0,30,10):
+    for j in range(4,14):
+        bilangan = (i + j) * 30
+        print(format(bilangan,"5d"), end = '')
+    print("\n")
 
-while bilangan <= 1000:
-    if bilangan % 5 == 0 and bilangan % 6 == 0:
-        print(format(bilangan,"4d"), end = '')
-    bilangan += 1
+print("----------------------------------------------------------")
