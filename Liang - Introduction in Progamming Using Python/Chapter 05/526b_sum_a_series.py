94be19638d8c20453746ffa815294653239644a6
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-24 17:40:01 +0700

    del 526b, del 527b, edit 526a

--- Liang - Introduction in Progamming Using Python/Chapter 05/526b_sum_a_series.py
+++
@@ -1,8 +0,0 @@
-jumlah = 0
-
-for n in range(1,50):
-    suku = (2 * n - 1) / (2 * n + 1)
-    jumlah += suku
-    print("------------------------------")
-    print("Jumlah", n, "suku pertama adalah ", jumlah)
-    print("------------------------------")
