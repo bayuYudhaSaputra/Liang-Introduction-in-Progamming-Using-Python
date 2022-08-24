8c3d3795b506ef93e13a002edba9f8e0dda1582e
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-24 17:53:02 +0700

    edit 527a

--- Liang - Introduction in Progamming Using Python/Chapter 05/527a_compute_pi.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/527a_compute_pi.py
@@ -1,13 +1,13 @@
 # Menentukan aproksimasi nilai phi
 
-i = 1
+i = 10000
 jumlah = 0
 
-while i < 10000:
+while i > 0:
     suku = ((-1) ** (i + 1)) / (2 * i - 1)
     jumlah += suku
     phi = 4 * jumlah
-    i += 1
+    i -= 1
 
 print("\n-----------------------------------")
 print("Nilai pi = ", phi)
