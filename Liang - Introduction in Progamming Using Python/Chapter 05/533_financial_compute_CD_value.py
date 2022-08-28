d587305b559589cfeaaa826bebbf97ec90f221d7
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-28 07:59:41 +0700

    add 533

---
+++ Liang - Introduction in Progamming Using Python/Chapter 05/533_financial_compute_CD_value.py
@@ -0,0 +1,14 @@
+print("\n========================================")
+deposito = eval(input("Berapa nominal deposito yang disimpan? Rp."))
+bungaPersen = eval(input("Berapa persen bunga tahunan yang didapat? "))
+jangkaWaktu = eval(input("Berapa bulan jangka waktu deposito?"))
+
+print("\n---------------------------------------")
+print("|", format("Bulan ke","8s"), "|", format("Nilai Deposito","10s"), "|")
+print("---------------------------------------\n")
+for i in range(1, jangkaWaktu + 1):
+    bungaNominal = deposito * (bungaPersen / 1200)
+    deposito += bungaNominal
+    print("|", format(i,"8.0f"), "|", format(deposito,"14.2f"), "|")
+
+print("---------------------------------------\n")
