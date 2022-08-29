c3ffe27cad44c7353aac4575c8d0ad336d6f6f92
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-29 19:33:42 +0700

    add 529

--- Liang - Introduction in Progamming Using Python/Chapter 05/529_display_leap_year.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/529_display_leap_year.py
@@ -6,4 +6,3 @@ print("------------------------------------------------\n")
 for tahun in range(2001, 2101):
     if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
         print(tahun)
-    
