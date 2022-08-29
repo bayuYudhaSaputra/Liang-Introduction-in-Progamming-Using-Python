956699c9adbfb5157090172914dc3bd5c502c157
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-08-30 04:05:40 +0700

    edit 529

--- Liang - Introduction in Progamming Using Python/Chapter 05/529_display_leap_year.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/529_display_leap_year.py
@@ -1,8 +1,12 @@
 print("\n------------------------------------------------")
 print("Daftar Tahun Kabisat Mulai 2001 hingga 2100")
 print("------------------------------------------------\n")
-
-
+count = 0
 for tahun in range(2001, 2101):
+
     if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-        print(tahun)
+        print(tahun, end = ' ')
+        count += 1
+        if count % 10 == 0:
+            print("\n")
+print("\n------------------------------------------------\n")
