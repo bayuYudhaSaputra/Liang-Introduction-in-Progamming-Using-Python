fc9bb04a325d81f732f87f94356d5a057fa3c39e
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-09-01 19:08:39 +0700

    edit 503b

--- Liang - Introduction in Progamming Using Python/Chapter 05/53b_conversion_from_kilograms_to_pounds.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/53b_conversion_from_kilograms_to_pounds.py
@@ -19,7 +19,7 @@ for nomor in range(1,100):
     pound = 2.2 * kilogram
     
     print(
-    "  |", format(nomor,"3"), "|", format(kilogram,"3"), "|", format(pound, "6.2f"), "|"
+    "  |", format(nomor,"3d"), "|", format(kilogram,"3d"), "|", format(pound, "6.2f"), "|"
     )
 
     kilogram = 2 * nomor - 1 # rumus barisan bilangan ganjil
