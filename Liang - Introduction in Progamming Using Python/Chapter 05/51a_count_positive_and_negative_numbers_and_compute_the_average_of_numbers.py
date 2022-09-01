3f35c60a0f808e4baec48bd204bbdb6a88adcd35
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-09-01 10:54:51 +0700

    edit 501

--- Liang - Introduction in Progamming Using Python/Chapter 05/51a_count_positive_and_negative_numbers_and_compute_the_average_of_numbers.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/51a_count_positive_and_negative_numbers_and_compute_the_average_of_numbers.py
@@ -13,17 +13,20 @@ sum = 0
 banyakPerulangan = 0
 banyakNegatif = 0
 
-while inputBilangan != 0:
-    sum += inputBilangan
-    banyakPerulangan += 1 # menghitung banyak perulangan
-    rata2 = sum / banyakPerulangan
+if inputBilangan == 0:
+    print("Anda tidak menginput bilangan sama sekali.")
+else:
+    while inputBilangan != 0:
+        sum += inputBilangan
+        banyakPerulangan += 1 # menghitung banyak perulangan
+        rata2 = sum / banyakPerulangan
     
-    if inputBilangan < 0:
-        banyakNegatif += 1 # menghitung banyak bilangan negatif yang diinput
+        if inputBilangan < 0:
+            banyakNegatif += 1 # menghitung banyak bilangan negatif yang diinput
     
-    inputBilangan = eval(input("Input bilangan integer" + 
+        inputBilangan = eval(input("Input bilangan integer" + 
                 "(program akan berhenti jika anda menginput 0) : "))
 
-print("Jumlah bilangan yang diinput adalah", sum)
-print("Rata-rata bilangan yang diinput adalah", rata2)
-print("Banyak bilangan negatif yang diinput adalah", banyakNegatif)
+    print("Jumlah bilangan yang diinput adalah", sum)
+    print("Rata-rata bilangan yang diinput adalah", rata2)
+    print("Banyak bilangan negatif yang diinput adalah", banyakNegatif)
