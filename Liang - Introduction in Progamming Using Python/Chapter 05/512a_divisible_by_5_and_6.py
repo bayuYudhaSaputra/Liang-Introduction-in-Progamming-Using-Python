deedec44498071bff95b4bc1daf5f329f403b7d4
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-09-06 00:24:34 +0700

    edit 512a

--- Liang - Introduction in Progamming Using Python/Chapter 05/512a_divisible_by_5_and_6.py
+++ Liang - Introduction in Progamming Using Python/Chapter 05/512a_divisible_by_5_and_6.py
@@ -1,11 +1,15 @@
 print("----------------------------------------------------------\n",
     "Bilangan antara 100 hingga 1000 yang habis dibagi 5 dan 6\n",
-    "----------------------------------------------------------"
+    "------------------------------------------------------------"
     )
-for i in range(0,30,10):
-    for j in range(4,14):
-        bilangan = (i + j) * 30
-        print(format(bilangan,"5d"), end = '')
-    print("\n")
+count = 0
+for bilangan in range(100, 1000 + 1):
+    if bilangan % 5 == 0 and bilangan % 6 == 0:
+        # bilangan habis dibagi 5 dan 6
+        print(format(bilangan, "5d"), end = ' ')
+        count += 1
+        if count % 10 == 0:
+            # Setiap baris hanya berisi 10 bilangan
+            print("\n")
 
 print("----------------------------------------------------------")
---
+++ Liang - Introduction in Progamming Using Python/Chapter 05/517_display_the_ASCII_character_table.py
@@ -0,0 +1,7 @@
+for i in range(33,127):
+    kodeAscii = chr(i)
+    print(kodeAscii)
+    count = 1
+    if count % 10 == 0:
+        print("\n")
+        count += 1
