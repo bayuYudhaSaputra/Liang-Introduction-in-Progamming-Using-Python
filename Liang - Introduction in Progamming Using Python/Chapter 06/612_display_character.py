970bdf1889247aa4ecf84d4888b7b9f4585ab4b4
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-10-27 20:03:31 +0700

    edit 612

--- Liang - Introduction in Progamming Using Python/Chapter 06/612_display_character.py
+++ Liang - Introduction in Progamming Using Python/Chapter 06/612_display_character.py
@@ -1,15 +1,15 @@
 # define character function
 def printChars(ch1, ch2, numberPerLine):
-    
-    while ch1 < ch2:
-        j = 0
+    while ch1 < ch2: # ketika ch1 < ch2, looping dijalankan
+        j = 0 # j diberi nilai 0
         while j < numberPerLine:
+            # ketika j < nilai dalam numberPerline, looping dijalankn
             print(format(str(chr(ch1)),"4s"), end = " ")
             j += 1
             ch1 += 1
-            if ch1 > ch2:
-                break
+            if ch1 > ch2: 
+                break 
+                # ketika nilai ch1 > ch2 , program langsung dihentikan walaupun looping belum selesai
         print("\n")
-    
-        
+           
 printChars(49, 90, 10)
