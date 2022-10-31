70476f12a92736dedc2035c7a8053dbbe7d58df7
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2022-10-31 17:51:25 +0700

    edit 614

--- Liang - Introduction in Progamming Using Python/Chapter 06/614_estimate_phi.py
+++ Liang - Introduction in Progamming Using Python/Chapter 06/614_estimate_phi.py
@@ -1,19 +1,19 @@
 # define number sequence
 def numberSequence(i):
-    mi = ((-1) ** (i + 1)) / (2 * i - 1)
-    return mi
+    ui = ((-1) ** (i + 1)) / (2 * i - 1)
+    return ui
 
 # define main function
 def main():
     numberSeries = 0
     print("===============================================")
-    print("||", format("i", "2s"), "|", format("m(i)", "6s"), "||")
+    print("||", format("i", "3s"), "|", format("m(i)", "6s"), "||")
     print("-----------------------------------------------")
     for n in range(1, 1000, 100):
         # count sum of sequence number
         numberSeries += numberSequence(n)
-        phi = 4 * numberSeries
-        print("||", format(n, "2d"), "|", format(phi, "6.3f"), "||")
+        mi = 4 * numberSeries
+        print("||", format(n, "3d"), "|", format(mi, "6.3f"), "||")
     print("===============================================")
 
 main()
