bc45e4e7c17b8a390de614633b0a650ebe60fcdb
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2023-01-07 00:34:42 +0700

    add 606

---
+++ Liang - Introduction in Progamming Using Python/Chapter 06/606_display_pattern.py
@@ -0,0 +1,15 @@
+# define displayPattern function
+def displayPattern(n):
+    for numColomn in range(1, n + 1):
+    # membuat kolom
+        for numRow in range(numColomn, 0, -1):
+        # menampilkan bilangan dalam baris
+            print(numRow, end = " ")
+        print(" ")
+    return "" # agar tidak mengembalikan nilai none
+
+def main():
+    n = eval(input("Input number :"))
+    print(displayPattern(n))
+
+main()
