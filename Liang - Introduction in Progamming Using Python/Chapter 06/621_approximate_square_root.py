0ef234716890e6365b0db920d8160f0f6c4e9030
bayuYudhaSaputra <bayubayuyudhasaputraqed@gmail.com>
2023-01-15 20:18:59 +0700

    edit 621

--- Liang - Introduction in Progamming Using Python/Chapter 06/621_approximate_square_root.py
+++ Liang - Introduction in Progamming Using Python/Chapter 06/621_approximate_square_root.py
@@ -2,12 +2,23 @@
 # Memperkirakan hasil akar kuadrat menggunakan metode Babylonia
 # ==============================================================
 
-n = 5
-lastGuess = 1
-nextGuess = (lastGuess + (n / lastGuess)) / 2
+def sqrt(n):
 
-print("|", "lastGuess", "|", "nextGuess", "|")
-while (nextGuess - lastGuess > 0.000000001):
+    lastGuess = 1
+    # diberi nilai awal 1
     nextGuess = (lastGuess + (n / lastGuess)) / 2
-    print("|", lastGuess, "|", nextGuess, "|")
-    lastGuess += 1
+    # rumus aproksimasi akar babylonia
+
+    while (nextGuess - lastGuess > 0.000000001):
+    # ketika selisih nextGuess dengan lastGuess lebih dari 0.000000001
+    # hitung nextGuess hingga tidak memenuhi syarat.
+        nextGuess = (lastGuess + (n / lastGuess)) / 2
+        lastGuess += 1
+
+    return nextGuess
+
+def main():
+    n = eval(input("Input bilangan yang akan diakar : "))
+    print("Akar dari", n, "adalah :", sqrt(n))
+
+main()
